78d1380df5389f3649ded8a24f3970c9931d7138
talalzeini <56060993+talalzeini@users.noreply.github.com>
2020-04-17 13:28:18 -0700

    added contact form

--- script.js
+++ script.js
@@ -121,3 +121,38 @@ function download(filename, text) {
   download("index.html",eh);
   console.log("downloading")
   }
+
+
+
+  $(document).ready(function(){
+    $('.submit').click(function(event){
+    console.log('Clicked button')
+    
+    var name=$('.name').val()
+    var email =$('.email').val()
+    var subject =$('.subject').val()
+    var message =$('.message').val()
+    var statusElm =$('.status')
+    statusElm.empty()
+    
+    if(email.length > 5 && email.includes('@') && email.includes('.')){
+        statusElm.append('')
+     } else {
+        statusElm.append('<div style="color:red; text-align:center;">The email you entered is not valid.<br><br></div>')
+        event.preventDefault()
+        }
+    
+    
+    
+       if(message.length != 0){
+        statusElm.append('')
+    } else {
+       statusElm.append('<div style="color:red; text-align:center;">Your message is empty.<br><br></div>')
+       event.preventDefault()
+       }
+    
+    })
+    
+    })
+    
+
